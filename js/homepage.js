fda245ae11fc070bd9631f5163c88ccc3db9bf15
davidKH <davidjb06.kodehode@gmail.com>
2023-10-19 17:54:49 +0200

    started site

---
+++ js/homepage.js
@@ -0,0 +1,18 @@
+const userCard = document.querySelector("User-cards");
+
+const names = ["Anna", "Roger", "Mina"];
+
+const titles = ["mmmmm, tasty", "You gotta try this", "See what i made"];
+
+function createCards() {
+  let randomCard = Math.floor(Math.random() * 3);
+  userCard.innerHTML += `
+<img src="" alt="User" />
+<h2>${names[randomCard]}</h2>
+<div class="conainer">
+<img src="" alt="" />
+<h3>${titles[randomCard]}</h3>
+<p></p>
+</div>
+`;
+}

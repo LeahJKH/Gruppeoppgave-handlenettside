4093a8d17fd41c0f4c62ddd56018c4ef9d1f861e
davidKH <davidjb06.kodehode@gmail.com>
2023-10-23 11:20:46 +0200

    made everything uniform and added the others code

--- js/homepage.js
+++ js/homepage.js
@@ -1,77 +1,76 @@
-const userCard = document.querySelector("User-cards");
-// Arrays
-const users = [
-  {
-    name: "Anna",
-    title: "mmmmm, tasty",
-    time: "10 min",
-    image: { src: "hamburger.avif", title: "Hamburger title" },
-  },
-  {
-    name: "Roger",
-    title: "You gotta try this",
-    time: "20 min",
-    image: { src: "pasta.avif", title: "Pasta title" },
-  },
-  {
-    name: "Mina",
-    title: "See what I made",
-    time: "45 min",
-    image: { src: "pizza.avif", title: "Pizza title" },
-  },
-  {
-    name: "Hans",
-    title: "Easy and fast",
-    time: "60 min",
-    image: { src: "Salad.avif", title: "Salad title" },
-  },
-];
-//  Function to create a user card
-function createUserCard(user) {
-  const randomCardIndex = Math.floor(Math.random() * names.length);
-  const user = users[randomCardIndex];
+document.addEventListener("DOMContentLoaded", function () {
+  const userCard = document.querySelector("#user-cards"); // Corrected the selector
 
-  userCard.innerHTML = `
-    <img src=${image[randomCardIndex].src} alt="User" class="user-img" />
-    <h2>${image[randomCardIndex].title}</h2>
+  //!! current pages !!//
+  const pages = {
+    homePage: document.querySelector("#home-page"),
+    shoppingCartPage: document.querySelector("#shopping-page"),
+    userPage: document.querySelector("#user-page"),
+    listpage: document.querySelector("#list-page"),
+  };
+  //!! current pages !!//
+
+  const users = [
+    {
+      name: "Anna",
+      title: "mmmmm, tasty",
+      time: "10 min",
+      image: { src: "./images/hamburger.avif", title: "Hamburger title" },
+    },
+    {
+      name: "Roger",
+      title: "You gotta try this",
+      time: "20 min",
+      image: { src: "./images/pasta.avif", title: "Pasta title" },
+    },
+    {
+      name: "Mina",
+      title: "See what I made",
+      time: "45 min",
+      image: { src: "./images/pizza.avif", title: "Pizza title" },
+    },
+    {
+      name: "Hans",
+      title: "Easy and fast",
+      time: "60 min",
+      image: { src: "./images/Salad.avif", title: "Salad title" },
+    },
+  ];
+
+  function createUserCard() {
+    const randomCardIndex = Math.floor(Math.random() * users.length);
+
+    const userCardElement = document.createElement("div");
+    userCardElement.innerHTML = `
+    <img src="${users[randomCardIndex].image.src}" alt="User" class="user-img" />
+    <h2>${users[randomCardIndex].image.title}</h2>
     <div class="container">
-      }
-      <h3>${titles[randomCardIndex]}</h3>
-      <p>${time[randomCardIndex]}</p>
+      <h3>${users[randomCardIndex].title}</h3>
+      <p>${users[randomCardIndex].time}</p>
     </div>
   `;
-  userCard.appendChild(userCard);
-}
 
-// Create multiple user cards
-for (let i = 0; i < 4; i++) {
-  createUserCard();
-}
-// Test
-//
-// userCard.innerHTML = `
-//   <img src="${user.image.src}" alt="User" class="user-img" />
-//   <h2>${user.name}</h2>
-//   <div class="container">
-//     ${createImageElement(user.image.src, user.image.title).outerHTML}
-//     <h3>${user.title}</h3>
-//     <p>${user.time}</p>
-//   </div>
-// `;
+    userCard.appendChild(userCardElement);
+  }
 
-// PAGE SWITCHERS//
-const homePage = document.querySelector("#home-page");
-const shoppingCartPage = document.querySelector("#shopping-page");
-const userPage = document.querySelector("#user-page");
+  // Create multiple user cards
+  for (let i = 0; i < 4; i++) {
+    createUserCard();
+  }
 
-//const listpage = document.querySelector("#list-page").addEventListener("click", function () {});;
-homePage.addEventListener("click", function () {
-  location.href = "./homepage.html";
+  //!! Page switchers !!//
+  //const recepice = document.querySelector("#recepiece-page").addEventListener("click", function () {});;
+  pages.listpage.addEventListener("click", function () {
+    location.href = "./list.html";
+  }); // makes the button clickable and changes page
+  pages.homePage.addEventListener("click", function () {
+    location.href = "./homepage.html";
+  }); // makes the button clickable and changes page
+  pages.shoppingCartPage.addEventListener("click", function () {
+    location.href = "./handlekurv.html";
+  }); // makes the button clickable and changes page
+  pages.userPage.addEventListener("click", function () {
+    location.href = "./userpage.html";
+  }); // makes the button clickable and changes page
+  //!! Page switchers !!//
 });
-shoppingCartPage.addEventListener("click", function () {
-  location.href = "./handlekurv.html";
-});
-userPage.addEventListener("click", function () {
-  location.href = "./userpage.html";
-});
-//const recepice = document.querySelector("#recepiece-page").addEventListener("click", function () {});;
